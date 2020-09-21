6177c68a6b774b9149065b1d72944ffd6b3b37fe
tlott99 <travis.lott@svu.edu>
2020-09-21 10:26:50 -0400

    1st version

---
+++ src/com/company/Coat.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Coat {
+    private String mainColor = "Blue";
+    public Coat(String mainColor){
+        this.mainColor = mainColor;
+    }
+    public Coat(){
+        mainColor = mainColor;
+    }
+    public String getColor(){
+        return mainColor;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String name = "Peanuts";
-        Boat boat = new Boat("John", 5);
-        System.out.println(boat.getName());
-        boat.setName(name);
-        System.out.println(boat.getName());
-        name = "Lala";
-        boat.setName(name);
-        System.out.println(boat.getName());
-        System.out.println(boat.getLength());
+    Coat coat = new Coat();
+    System.out.println(coat.getColor());
     }
 }
